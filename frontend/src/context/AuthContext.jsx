c87d1f21e851286d074b73e8cbdcaa54c5a13a6f
mohdbilal123456 <111408918+mohdbilal123456@users.noreply.github.com>
2025-09-04 14:40:02 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createContext } from 'react'
 export const authDataContext= createContext()
 function AuthContext({children}) {
-    let serverUrl = "http://localhost:5000"
+    let serverUrl = "https://onecart-e-commerce-1.onrender.com"
 
     let value = {
        serverUrl
