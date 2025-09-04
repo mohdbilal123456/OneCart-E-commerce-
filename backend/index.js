2668efda9dce40d484b092c6c6ddf3c1bbfa8e29
mohdbilal123456 <111408918+mohdbilal123456@users.noreply.github.com>
2025-09-04 14:56:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["http://localhost:5173" , "http://localhost:5174"],
+ origin:["https://onecart-e-commerce-2.onrender.com" , "http://localhost:5174"],
  credentials:true
 }))
 
